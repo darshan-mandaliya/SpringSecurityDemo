c64851202ddd857b16fdab06651132d19f073e6a
darshan.mandaliya <darshan.mandaliya.specindia@gmail.com>
2022-08-17 15:04:48 +0530

    Initial Commit (Without Spring Security)

---
+++ SpringSecurityDemo/src/main/java/com/springsecuritydemo/SpringSecurityDemo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.springsecuritydemo.SpringSecurityDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
