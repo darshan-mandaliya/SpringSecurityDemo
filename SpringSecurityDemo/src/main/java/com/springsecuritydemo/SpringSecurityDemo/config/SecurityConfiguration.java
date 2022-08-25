3421b4054706ccc092907c15305824340991c0bf
darshan.mandaliya <darshan.mandaliya@spec-india.com>
2022-08-25 13:17:18 +0530

    Updated code for Authorization

--- SpringSecurityDemo/src/main/java/com/springsecuritydemo/SpringSecurityDemo/config/SecurityConfiguration.java
+++ SpringSecurityDemo/src/main/java/com/springsecuritydemo/SpringSecurityDemo/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.springsecuritydemo.SpringSecurityDemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -12,13 +13,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("darsh").password("darsh12").roles("USER").and().withUser("test")
-				.password("darsh123").roles("ADMIN");
+		auth.inMemoryAuthentication()
+		.withUser("darsh").password("darsh123").roles("USER").and()
+		.withUser("test").password("darsh123").roles("ADMIN");
 	}
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoderBean() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	// Authorization
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()		
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("USER","ADMIN")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
 	}
 	
 }
