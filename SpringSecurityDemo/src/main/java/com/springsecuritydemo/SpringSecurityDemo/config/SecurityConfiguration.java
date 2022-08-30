1961e615f13dad86533d0d742ed05fee072355b1
darshan.mandaliya <darshan.mandaliya@spec-india.com>
2022-08-30 15:46:48 +0530

    Implement JDBC Authentication with H2 Default Schema (Remove InMemory Authentication)

--- SpringSecurityDemo/src/main/java/com/springsecuritydemo/SpringSecurityDemo/config/SecurityConfiguration.java
+++ SpringSecurityDemo/src/main/java/com/springsecuritydemo/SpringSecurityDemo/config/SecurityConfiguration.java
@@ -1,21 +1,31 @@
 package com.springsecuritydemo.SpringSecurityDemo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("darsh").password("darsh123").roles("USER").and()
-		.withUser("test").password("darsh123").roles("ADMIN");
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.withDefaultSchema()
+		.withUser(User.withUsername("darsh").password("darsh123").roles("USER"))
+		.withUser(User.withUsername("test").password("darsh123").roles("ADMIN"));
 	}
 	
 	@Bean
